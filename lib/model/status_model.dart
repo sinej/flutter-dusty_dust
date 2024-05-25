6dce4e468f0095aef9162ef8e16293d823f50aa7
shin <sinej1020@gmail.com>
2024-05-25 21:21:56 +0900

    feat: status 추가

--- lib/model/status_model.dart
+++ lib/model/status_model.dart
@@ -1,3 +1,35 @@
-class StatusModel{
+import 'package:flutter/material.dart';
 
+class StatusModel{
+  final int level;
+  final String label;
+  final Color primaryColor;
+  final Color darkColor;
+  final Color lightColor;
+  final Color fontColor;
+  final String imagePath;
+  final String comment;
+  final double minPM10;
+  final double minPM20;
+  final double minO3;
+  final double minNO2;
+  final double minCO;
+  final double minSO2;
+
+  const StatusModel({
+    required this.level,
+    required this.label,
+    required this.primaryColor,
+    required this.darkColor,
+    required this.lightColor,
+    required this.fontColor,
+    required this.imagePath,
+    required this.comment,
+    required this.minPM10,
+    required this.minPM20,
+    required this.minO3,
+    required this.minNO2,
+    required this.minCO,
+    required this.minSO2,
+});
 }
